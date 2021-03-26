36ad3290c6258413bd02f28ba8ce2f52a0ff1046
MartinW <a7780117@gmail.com>
2021-03-26 13:26:21 +0800

    [Update] Chapter_6.1 Avater

---
+++ Chapter_6.1 Avater/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter_6._1_Avater
+{
+    class Person
+    {
+        private string name;
+        public Person(string name) 
+        {
+            this.name = name;
+        }
+        public void WearTShirts() 
+        {
+            Console.Write("大T恤 ");
+        }
+        public void WearBigTrouser() 
+        {
+            Console.Write("垮褲 ");
+        }
+        public void WearSheakers() 
+        {
+            Console.Write("破球鞋 ");
+        }
+        public void WearSuit() 
+        {
+            Console.Write("西裝 ");
+        }
+        public void WearTie() 
+        {
+            Console.Write("領帶 ");
+        }
+        public void WearLeatherShoes() 
+        {
+            Console.Write("皮鞋 ");
+        }
+        public void Show() 
+        {
+            Console.WriteLine("裝扮的{0}",name);
+        }
+    }
+}
---
+++ Chapter_6.1 Avater/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Chapter_6._1_Avater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person xc = new Person("小菜");
+            
+            Console.WriteLine("\n第一種裝扮:");
+
+            xc.WearTShirts();
+            xc.WearBigTrouser();
+            xc.WearSheakers();
+            xc.Show();
+
+            Console.WriteLine("\n第二種裝扮:");
+
+            xc.WearSuit();
+            xc.WearTie();
+            xc.WearLeatherShoes();
+            xc.Show();
+
+
+        }
+    }
+}
